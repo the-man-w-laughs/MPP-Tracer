be76fa97e27a9dae737c82b933f481aa8731a182
Nazar <nazarmasukov@gmail.com>
2022-09-20 17:27:16 +0300

    Second test added.

--- Tracer/Tracer.Core.Tests/TestClasses.cs
+++ Tracer/Tracer.Core.Tests/TestClasses.cs
@@ -31,3 +31,31 @@ public class B1{
         _tracer.StopTrace();
     }
 }
+
+public class A2{
+    private ITracer _tracer;
+
+    public A2(ITracer tracer){
+        this._tracer = tracer;
+    }
+
+    public void a(){
+        _tracer.StartTrace();
+        Thread.Sleep(100);
+        _tracer.StopTrace();
+    }
+}
+
+public class B2{
+    private ITracer _tracer;
+
+    public B2(ITracer tracer){
+        this._tracer = tracer;
+    }
+
+    public void b(){
+        _tracer.StartTrace();
+        Thread.Sleep(100);
+        _tracer.StopTrace();
+    }
+}
--- Tracer/Tracer.Core.Tests/Tests.cs
+++ Tracer/Tracer.Core.Tests/Tests.cs
@@ -31,27 +31,45 @@ public class Test
     }
 
 
-    // [Fact]
-    // public void MultiThreadWithNoInnerMethods()
-    // {
-    //     // Arrange
-    //     ITracer tracer = new Tracer.Core.Tracer();
-    //     A a = new A(tracer);
-    //     B b = new B(tracer);
+    [Fact]
+    public void MultiThreadWithNoInnerMethods()
+    {
+        // Arrange
+        Tracer tracer = new();
+        A2 a = new A2(tracer);
+        B2 b = new B2(tracer);
 
-    //     // Act
-    //     var t1 = new Thread(() =>
-    //     {
-    //         a.A2();
-    //     });
+        // Act
+        var t1 = new Thread(() =>
+        {
+            a.a();
+        });
 
-    //     var t2 = new Thread(() =>
-    //     {
-    //         b.B2();
-    //     });
+        var t2 = new Thread(() =>
+        {
+            b.b();
+        });
 
-    //     t1.Start();
-    //     t2.Start();
-    //     t1.Join();
-    //     t2.Join();}   
+        int id1 = t1.ManagedThreadId;
+        int id2 = t2.ManagedThreadId;               
+
+        t1.Start();
+        t2.Start();
+        t1.Join();
+        t2.Join(); 
+
+        // Assert
+        var traceResult = tracer.GetTraceResult().Threads;
+        Assert.Equal(traceResult.Count,2); 
+
+        Assert.Equal(traceResult[id1].Methods.Count,1);
+        Assert.Equal(traceResult[id1].Methods[0].ClassName,"A2");
+        Assert.Equal(traceResult[id1].Methods[0].MethodName,"a");
+        Assert.InRange<long>(traceResult[id1].Methods[0].StopWatch.ElapsedMilliseconds,100,120);
+
+        Assert.Equal(traceResult[id2].Methods.Count,1);
+        Assert.Equal(traceResult[id2].Methods[0].ClassName,"B2");
+        Assert.Equal(traceResult[id2].Methods[0].MethodName,"b");
+        Assert.InRange<long>(traceResult[id2].Methods[0].StopWatch.ElapsedMilliseconds,100,120);
+    }   
 } 
