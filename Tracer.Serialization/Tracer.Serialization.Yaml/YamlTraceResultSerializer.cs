3176365a1ee95ffac4e20779bc2d776075d54ee7
Nazar <nazarmasukov@gmail.com>
2022-09-20 03:05:08 +0300

    Yaml & Xml serializers have been implemented.

---
+++ Tracer.Serialization/Tracer.Serialization.Yaml/YamlTraceResultSerializer.cs
@@ -0,0 +1,81 @@
+﻿namespace Tracer.Serialization.Json;
+
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+
+public class YamlTraceResultSerializer : ITraceResultSerializer
+{
+    public string Format{ 
+        get {
+            return "Json";
+        }
+    }
+
+    private class _ThreadInfo{
+
+        public _ThreadInfo(int id,long time,List<_MethodInfo> methods){
+            this.id = id;
+            this.time = time;
+            this.methods = methods;
+        }
+
+        public int id;
+
+        public long time;
+
+        public List<_MethodInfo> methods;        
+    
+    }
+
+    private class _MethodInfo{
+
+        public _MethodInfo(MethodInfo method){
+
+            this.name = method.MethodName;
+            this.Class = method.ClassName;
+            this.time = $"{method.StopWatch.ElapsedMilliseconds}ms";
+            if (method.Methods != null){
+                this.methods = new List<_MethodInfo>();
+                foreach (var currMethod in method.Methods){
+                    this.methods.Add(new _MethodInfo(currMethod));
+                }
+            }
+            else {
+                this.methods = null;
+            }
+        }
+        public String name;
+
+        [JsonPropertyName("class")]
+        public String Class;
+
+        public String time;
+
+        public List<_MethodInfo>? methods;
+    }
+
+    private List<_ThreadInfo> _ThreadResultToList(TraceResult traceResult){
+        List<_ThreadInfo> resultList= new();
+        List<_MethodInfo> methods;
+        long time;
+        for (int i = 0; i < traceResult.Threads.Count; i++){  
+            methods = new();
+            time = 0;
+            foreach (var method in traceResult.Threads[i].Methods){
+                methods.Add(new _MethodInfo(method));
+                time += method.StopWatch.ElapsedMilliseconds;
+            }
+            resultList.Add(new _ThreadInfo(i,time,methods));
+        }
+        return resultList;
+    }  
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        List<_ThreadInfo> serializableList = _ThreadResultToList(traceResult);
+        var res = JsonSerializer.Serialize<List<_ThreadInfo>>(serializableList);
+        to.Write(Encoding.Default.GetBytes(res));
+    }
+}
